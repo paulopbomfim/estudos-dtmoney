8cc7c58fc375fa92db0a1e56761ae281de0d49aa
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-02-04 21:16:53 -0300

    :construction: stylization of modal inputs and button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
 
+import CloseImg from '../../assets/close.svg';
+
+import { Container } from './styles';
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,8 +14,39 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   <Modal
     isOpen={ isOpen }
     onRequestClose={ onRequestClose }
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
   >
-    <h2>Cadastrar nova transação</h2>
+
+    <button
+      type="button"
+      onClick={ onRequestClose }
+      className="react-modal-close"
+    >
+      <img src={ CloseImg } alt="Fechar modal" />
+    </button>
+
+    <Container>
+      <h2>Cadastrar transação</h2>
+      <input
+        type="text"
+        placeholder="Título"
+      />
+
+      <input
+        type="number"
+        placeholder="Valor"
+      />
+
+      <input
+        type="text"
+        placeholder="Categoria"
+      />
+      
+      <button type="submit">
+        Cadastrar
+      </button>
+    </Container>
   </Modal>
   );
 };
